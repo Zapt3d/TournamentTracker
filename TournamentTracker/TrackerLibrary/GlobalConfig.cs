9b6504a432964560e98f7577ac2d51267d528747
Zapt3d <aleksi.toivonen@protonmail.com>
2022-06-30 11:36:21 +0300

    Add mock connectors to GlobalConfig

--- TournamentTracker/TrackerLibrary/GlobalConfig.cs
+++ TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -8,18 +8,22 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; }
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
 
         public static void InitializeConnections(bool database, bool textFiles)
         {
             if (database)
             {
-                //TODO: Create the SQL Connection
+                //TODO: Set up the SQL connector properly
+                SqlConnector sql = new();
+                Connections.Add(sql);
             }
 
             if (textFiles)
             {
                 //TODO: Create the text file
+                TextConnector text = new();
+                Connections.Add(text);
             }
 
         }
