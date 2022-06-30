9390b50b09f05ecb820c14dfb9c3c39755edc2cd
Zapt3d <aleksi.toivonen@protonmail.com>
2022-06-30 11:38:45 +0300

    Add form validation to CreatePrizeForm

--- TournamentTracker/TrackerUI/CreatePrizeForm.cs
+++ TournamentTracker/TrackerUI/CreatePrizeForm.cs
@@ -16,5 +16,58 @@ namespace TrackerUI
         {
             InitializeComponent();
         }
+
+        private void CreatePrizeButton_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+
+            }
+        }
+        /// <summary>
+        /// Validates all field inputs by user
+        /// </summary>
+        /// <returns><c>true</c> if all form fields are valid; <c>false</c> if one or more form field(s) are invalid</returns>
+        private bool ValidateForm()
+        {
+            bool validForm = true;
+            
+            // Placenumber needs to be a valid int
+            if (!int.TryParse(placeNumberValue.Text, out int placeNumber))
+            {
+                validForm = false;
+            }
+            // Placenumber needs to be > 1
+            if (placeNumber < 1)
+            {
+                validForm = false;
+            }
+            // Placename cant be empty
+            if (placeNameValue.Text.Length == 0)
+            {
+                validForm = false;
+            }
+            
+            bool prizeAmountValid = decimal.TryParse(prizeAmountValue.Text, out decimal prizeAmount);
+            bool prizePercentageValid = !int.TryParse(pricePercentageValue.Text, out int prizePercentage);
+
+            // Either prize amount or prize percentage needs to be valid
+            if (!prizeAmountValid || !prizePercentageValid)
+            {
+                validForm = false;
+            }
+            // prize amount and prize percentage need to be equal or greater than 0
+            if (prizeAmount <= 0 && prizePercentage <= 0)
+            {
+                validForm = false;
+            }
+            // prize percentage cant be above 100 or below 0
+            if (prizePercentage > 100 || prizePercentage < 0)
+            {
+                validForm = false;
+            }
+
+            return validForm;
+        }
     }
 }
