4f5bf1dce820294853b7fc3efb2d97b006295365
Zapt3d <aleksi.toivonen@protonmail.com>
2022-07-01 13:43:10 +0300

    Create helper extension class for TextConnector and logic for Prize saving to text file

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TrackerLibrary.Models;
+﻿using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextExtensions;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
         /// TODO: Make the CreatePrize method actually save to the text file
         /// <summary>
         /// Saves a new prize to the text file
@@ -17,7 +15,23 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load csv file and convert to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find next available Id, set to one if the file has no records/does not exist
+            int nextAvailableId = 1;
+
+            if (prizes.Count > 0) 
+            {
+                nextAvailableId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            // Add Id to model and add model to List<PrizeModel>
+            model.Id = nextAvailableId;
+            prizes.Add(model);
+
+            // Convert List<PrizeModel> back to csv and write to file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
---
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,83 @@
+﻿using System.Configuration;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess.TextExtensions
+{
+    public static class TextConnectorProcessor
+    {
+        /// <summary>
+        /// Concatenates filename and App.config filepath to a full file path
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns>
+        /// full filepath as <c>string</c> to specified file
+        /// </returns>
+        public static string FullFilePath(this string fileName)
+        {
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{fileName}";
+        }
+
+        /// <summary>
+        /// Checks if specified file exists and loads the file content to a list
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>
+        /// Exists: contents of the file in list<br></br>
+        /// Does not exist: new empty list
+        /// </returns>
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        /// <summary>
+        /// Converts the text to List<PrizeModel>
+        /// </summary>
+        /// <param name="lines"></param>
+        /// <returns>
+        /// <c>List<PrizeModel></c> that was converted from lines
+        /// </returns>
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> prizeModels = new();
+
+            foreach(string line in lines)
+            {
+                string[] columns = line.Split(',');
+
+                PrizeModel prizeModel = new()
+                {
+                    Id = int.Parse(columns[0]),
+                    PlaceNumber = int.Parse(columns[1]),
+                    PlaceName = columns[2],
+                    PrizeAmount = decimal.Parse(columns[3]),
+                    PrizePercentage = double.Parse(columns[4])
+                };
+                prizeModels.Add(prizeModel);
+            }
+            return prizeModels;
+        }
+
+        /// <summary>
+        /// Saves changed prizemodels to the csv file
+        /// </summary>
+        /// <param name="models"></param>
+        /// <param name="fileName"></param>
+        public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new();
+
+            foreach (PrizeModel model in models)
+            {
+                lines.Add($"{model.Id},{model.PlaceNumber},{model.PlaceName},{model.PrizeAmount},{model.PrizePercentage}");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+    }
+}
